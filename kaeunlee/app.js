83f0f9943a63759ed42cb31ee9684147a09b5dc9
kaeun <kaeun@kaeunui-MacBookAir.local>
2022-11-24 15:44:41 +0900

    [40기 - 이가은] Assignment 1: Express API - 초기 환경 세팅

---
+++ kaeunlee/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+const { DataSource } = require("typeorm");
+const myDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+});
+myDataSource.initialize().then(() => {
+  console.log("Data Source has been initialized!");
+});
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+app.get("/ping", (req, res) => {
+  res.status(200).json({ message: "pong" });
+});
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
+};
+start();
