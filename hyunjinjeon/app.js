740e418215b44f7e20371e87234900a9ab715011
hjcod <hjjeon5435@gmail.com>
2022-11-25 19:07:01 +0900

    [40기-전형진]Assignment:Express API - 전체 게시글 조회하기

--- hyunjinjeon/app.js
+++ hyunjinjeon/app.js
@@ -1,15 +1,21 @@
-
+//Built-in
 const http =require ("http");
+
+//Third-party
 const express = require("express");
 const cors = require("cors");
 const morgan =  require("morgan");
 const dotenv = require("dotenv");
-
 const { DataSource } = require('typeorm'); 
 
+//Custom
+app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
 dotenv.config()
 
-const myDataSource = new DataSource({
+const appDataSource = new DataSource({
     type: process.env.TYPEORM_CONNECTION,
     host: process.env.TYPEORM_HOST,
     port: process.env.TYPEORM_PORT,
@@ -17,35 +23,34 @@ const myDataSource = new DataSource({
     password: process.env.TYPEORM_PASSWORD,
     database: process.env.TYPEORM_DATABASE
 })
-myDataSource.initialize()
+
+//Error handling
+appDataSource.initialize()
   .then(()=>{
     console.log("Data Source has been initialized!")
   });
 
-app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(morgan('combined'));
-
-
+// User signup endpoint
 app.post('/signup',async(req,res)=>{
     const {name,email,password,age} = req.body
-    await myDataSource.query(
+    await appDataSource.query(
         `INSERT INTO users (
-          name,
-          email,
-          password,
-          age
-       ) VALUES(?,?,?,?);
+          user_name,
+          user_email,
+          user_password,
+          user_age
+       ) VALUES(?,?,?,?); 
         `,[name,email,password,age]);
 
     res.status(200).json({message:"userCreated"});
 })
 
+
+// Posting endpoint
 app.post('/posts',async(req,res)=>{
     const {title,content} = req.body
-    await myDataSource.query(
+    await appDataSource.query(
       `INSERT INTO posts (
         title,
         content
@@ -54,13 +59,28 @@ app.post('/posts',async(req,res)=>{
     res.status(200).json({message:"postCreated"});
  })
 
+
+
+ app.get('/lookposts',async(req,res)=>{
+  await appDataSource.query(
+    `SELECT
+          users.user_name,
+          posts.title,
+          posts.created_at
+      FROM postss
+      LEFT JOIN users ON users.user_id = posts.user_id;
+      `,(err,rows)=>
+      {res.status(200).json(rows);});
+  });
+  
+
+
+
+
 const server = http.createServer(app);
 const PORT = process.env.PORT;
 
 const start = async () =>{
     server.listen(PORT, ()=> console.log(`server is listening to ${PORT}`))
 }
-
 start()
-
-
