049af92091fa6c8a065732395fd93f97b10758b4
hjcod <hjjeon5435@gmail.com>
2022-11-28 10:32:24 +0900

    [40기-전형진]Assignment1: Express API 초기환경세팅 최종수정본

---
+++ hyunjinjeon/db/migrations/20221127063800_create_post_table.sql
@@ -0,0 +1,17 @@
+-- migrate:up
+
+    CREATE TABLE posts
+    (
+        id INT NOT NULL AUTO_INCREMENT,
+        title VARCHAR(50) NOT NULL,
+        content VARCHAR(1000) NOT NULL,
+        user_id INT NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (user_id) REFERENCES users(id), 
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+
+-- migrate:down
+ DROP TABLE posts;
+
