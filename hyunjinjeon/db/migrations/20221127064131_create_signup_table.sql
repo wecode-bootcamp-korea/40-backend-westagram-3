5da62804f6d2d2d6acb3e6ed1168dcda5f14b6cb
hjcod <hjjeon5435@gmail.com>
2022-11-29 13:57:15 +0900

    [40기-전형진]Assignment1 : Express API 초기환경세팅 indention 재설정

--- hyunjinjeon/db/migrations/20221127064131_create_signup_table.sql
+++ hyunjinjeon/db/migrations/20221127064131_create_signup_table.sql
@@ -1,10 +1,8 @@
 -- migrate:up
 
-    INSERT INTO users (name,eamil,password,profile_image) VALUES ('CUP','CUP123@email.com','123cup','cupimage.url');
-
-    INSERT INTO users (name,eamil,password,profile_image) VALUES ('BOTTLE','BOT123@email.com','456boT','boTimage.url');
-
-    INSERT INTO users (name,eamil,password,profile_image) VALUES ('CHARGER','CHAR123@email.com','789ch','CHARimage.url');
+INSERT INTO users (name,eamil,password,profile_image) VALUES ('CUP','CUP123@email.com','123cup','cupimage.url');
+INSERT INTO users (name,eamil,password,profile_image) VALUES ('BOTTLE','BOT123@email.com','456boT','boTimage.url');
+INSERT INTO users (name,eamil,password,profile_image) VALUES ('CHARGER','CHAR123@email.com','789ch','CHARimage.url');
 
 -- migrate:down
 
--- hyunjinjeon/db/migrations/20221127064632_create_post_table.sql
+++ hyunjinjeon/db/migrations/20221127064632_create_post_table.sql
@@ -1,11 +1,11 @@
 -- migrate:up
-    
-    INSERT INTO posts (title,content,user_id) VALUES ('postingTitle1','postingContent1',1);
-    INSERT INTO posts (title,content,user_id) VALUES ('postingTitle2','postingContent2',1);
-    INSERT INTO posts (title,content,user_id) VALUES ('postingTitle3','postingContent3',1);
-    INSERT INTO posts (title,content,user_id) VALUES ('postingTitle4','postingContent4',2);
-    INSERT INTO posts (title,content,user_id) VALUES ('postingTitle5','postingContent5',2);
-    INSERT INTO posts (title,content,user_id) VALUES ('postingTitle6','postingContent6',3);
+
+INSERT INTO posts (title,content,user_id) VALUES ('postingTitle1','postingContent1',1);
+INSERT INTO posts (title,content,user_id) VALUES ('postingTitle2','postingContent2',1);
+INSERT INTO posts (title,content,user_id) VALUES ('postingTitle3','postingContent3',1);
+INSERT INTO posts (title,content,user_id) VALUES ('postingTitle4','postingContent4',2);
+INSERT INTO posts (title,content,user_id) VALUES ('postingTitle5','postingContent5',2);
+INSERT INTO posts (title,content,user_id) VALUES ('postingTitle6','postingContent6',3);
     
 -- migrate:down
 
